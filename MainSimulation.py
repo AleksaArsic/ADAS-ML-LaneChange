bd47bb9aacb260c0dcd0cda73d27bbafc0b787ae
Aleksa Arsic <arsicaleksa96@gmail.com>
2021-12-30 00:07:05 +0100

    Initial folder structure and base scripts.

---
+++ MainSimulation.py
@@ -0,0 +1,166 @@
+"""
+Script that render multiple rgb cameras in the same pygame window
+
+By default, it renders four cameras, left mirror, right mirror,
+center and back
+"""
+
+import glob
+import os
+import sys
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+import carla
+import argparse
+import random
+import time
+import numpy as np
+
+
+try:
+    import pygame
+    from pygame.locals import K_ESCAPE
+    from pygame.locals import K_q
+except ImportError:
+    raise RuntimeError('cannot import pygame, make sure pygame package is installed')
+
+""" Import all essential custom classes 
+"""
+from scripts.CustomTimer import CustomTimer
+from scripts.DisplayManager import DisplayManager
+from scripts.RGBCamera import RGBCamera
+
+def run_simulation(args, client):
+    """This function performed one test run using the args parameters
+    and connecting to the carla client passed.
+    """
+
+    display_manager = None
+    vehicle = None
+    vehicle_list = []
+    timer = CustomTimer()
+
+    try:
+
+        # Getting the world and
+        world = client.get_world()
+        original_settings = world.get_settings()
+
+        if args.sync:
+            traffic_manager = client.get_trafficmanager(8000)
+            settings = world.get_settings()
+            traffic_manager.set_synchronous_mode(True)
+            settings.synchronous_mode = True
+            settings.fixed_delta_seconds = 0.05
+            world.apply_settings(settings)
+
+
+        # Instanciating the vehicle to which we attached the sensors
+        bp = random.choice(world.get_blueprint_library().filter('vehicle.bmw.*'))
+        vehicle = world.spawn_actor(bp, random.choice(world.get_map().get_spawn_points()))
+        vehicle_list.append(vehicle)
+        vehicle.set_autopilot(True)
+
+        # Display Manager organize all the sensors an its display in a window
+        # If can easily configure the grid and the total window size
+        display_manager = DisplayManager(grid_size=[2, 3], window_size=[args.width, args.height])
+
+        # Then, RGBCamera can be used to spawn RGB Camera as needed
+        # and assign each to a grid position 
+        cam1 = RGBCamera(world, display_manager, carla.Transform(carla.Location(x=0, y=1.1, z=1.2), carla.Rotation(yaw=-180)), 
+                      vehicle, {}, display_pos=[0, 0])
+        cam2 = RGBCamera(world, display_manager, carla.Transform(carla.Location(x=0, z=1.7), carla.Rotation(yaw=+00)), 
+                      vehicle, {}, display_pos=[0, 1])
+        cam3 = RGBCamera(world, display_manager, carla.Transform(carla.Location(x=0, y=-1.1, z=1.2), carla.Rotation(yaw=180)), 
+                      vehicle, {}, display_pos=[0, 2])
+        cam4 = RGBCamera(world, display_manager, carla.Transform(carla.Location(x=-1.55, z=1.7), carla.Rotation(yaw=180)), 
+                      vehicle, {}, display_pos=[1, 1])
+
+        #Simulation loop
+        call_exit = False
+        time_init_sim = timer.time()
+        while True:
+            # Carla Tick
+            if args.sync:
+                world.tick()
+            else:
+                world.wait_for_tick()
+
+            # Render received data
+            display_manager.render()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    call_exit = True
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == K_ESCAPE or event.key == K_q:
+                        call_exit = True
+                        break
+
+            if call_exit:
+                break
+
+    finally:
+        if display_manager:
+            display_manager.destroy()
+
+        client.apply_batch([carla.command.DestroyActor(x) for x in vehicle_list])
+
+        world.apply_settings(original_settings)
+
+
+
+def main():
+    argparser = argparse.ArgumentParser(
+        description='CARLA Sensor tutorial')
+    argparser.add_argument(
+        '--host',
+        metavar='H',
+        default='127.0.0.1',
+        help='IP of the host server (default: 127.0.0.1)')
+    argparser.add_argument(
+        '-p', '--port',
+        metavar='P',
+        default=2000,
+        type=int,
+        help='TCP port to listen to (default: 2000)')
+    argparser.add_argument(
+        '--sync',
+        action='store_true',
+        help='Synchronous mode execution')
+    argparser.add_argument(
+        '--async',
+        dest='sync',
+        action='store_false',
+        help='Asynchronous mode execution')
+    argparser.set_defaults(sync=True)
+    argparser.add_argument(
+        '--res',
+        metavar='WIDTHxHEIGHT',
+        default='1280x720',
+        help='window resolution (default: 1280x720)')
+
+    args = argparser.parse_args()
+
+    args.width, args.height = [int(x) for x in args.res.split('x')]
+
+    try:
+        client = carla.Client(args.host, args.port)
+        client.set_timeout(5.0)
+
+        run_simulation(args, client)
+
+    except KeyboardInterrupt:
+        print('\nCancelled by user. Bye!')
+
+
+if __name__ == '__main__':
+
+    main()
